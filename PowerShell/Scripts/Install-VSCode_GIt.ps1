d043199ca7a21366c9baa610f9c77ee20b246187
Dave Carroll <powershell@anovelidea.org>
2018-09-01 09:17:03 -0500

    initial commit

---
+++ PowerShell/Scripts/Install-VSCode_GIt.ps1
@@ -0,0 +1,7 @@
+﻿Update-Module -Verbose
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Script -Name Install-VSCode,Install-Git
+
+Install-Git.ps1
+Install-VSCode.ps1 -AdditionalExtensions shan.code-settings-sync -LaunchWhenDone
