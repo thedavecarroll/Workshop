111dbdeaf7ca0ad6e3de0f89bfe684304d7479fb
Dave Carroll <powershell@anovelidea.org>
2018-09-03 16:08:06 -0500

    renamed file, add more things for it to do

--- PowerShell/Scripts/Install-VSCode_GIt.ps1
+++
@@ -1,7 +0,0 @@
-﻿Update-Module -Verbose
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-Install-Script -Name Install-VSCode,Install-Git
-
-Install-Git.ps1
-Install-VSCode.ps1 -AdditionalExtensions shan.code-settings-sync -LaunchWhenDone
---
+++ PowerShell/Scripts/SetupPowerShell.ps1
@@ -0,0 +1,13 @@
+﻿
+$ScriptsToInstall = 'Install-VSCode','Install-Git'
+$ModulesToInstall = 'PoShEvents','PoShGroupPolicy','ImportExcel','Plaster','PSake'
+
+Update-Module -Verbose
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+Install-Script -Name $ScriptsToInstall
+Install-Module -Name $ModulesToInstall
+
+Install-Git.ps1
+Install-VSCode.ps1 -AdditionalExtensions shan.code-settings-sync -LaunchWhenDone
