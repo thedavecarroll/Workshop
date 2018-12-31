d0b2c591b4cc1207043d40be9a0fafe7008c35d1
Dave Carroll <powershell@anovelidea.org>
2018-12-30 23:04:36 -0600

    support public and private unit tests, add tags Unit and $UnitType

--- PowerShell/Functions/Add-ModuleUnitTests.ps1
+++ PowerShell/Functions/Add-ModuleUnitTests.ps1
@@ -9,15 +9,20 @@ function Add-ModuleUnitTests {
 
     Pop-Location
 
+    $ProjectPath = Resolve-Path -Path $ProjectPath
+
     Set-Location -Path $ProjectPath
     Set-BuildEnvironment -Force
 
     $DefaultContent = @"
 
-Describe 'MODULECOMMAND' {
+Describe -Name 'MODULECOMMAND' -Tag 'Unit','UNITTYPE' {
 
     InModuleScope 'MODULENAME' {
 
+        It -Pending 'UnitTestPending' {
+
+        }
 
     }
 
@@ -25,16 +30,27 @@ Describe 'MODULECOMMAND' {
 
 "@
 
-    $ModuleFunctions = Get-ChildItem -Path $env:BHModulePath -File -Recurse -Include *.ps1
+    $BaseTestScriptPath = Join-Path -Path $ProjectPath -ChildPath 'Tests' | Join-Path -ChildPath 'Unit'
+
+    $ModuleFunctions = @()
+    $ModuleFunctions += Get-ChildItem -Path (Join-Path -Path $env:BHModulePath -ChildPath 'Public') -File -Recurse -Include *.ps1
+    $ModuleFunctions += Get-ChildItem -Path (Join-Path -Path $env:BHModulePath -ChildPath 'Private') -File -Recurse -Include *.ps1
+
     foreach ($Function in $ModuleFunctions) {
         . $Function.FullName
         $FunctionName = $Function.BaseName
         $Command = Get-Command -Name $FunctionName
         if ($Command.CommandType -eq 'Function') {
-            $TestScriptPath = Join-Path -Path $ProjectPath -ChildPath 'Tests' | Join-Path -ChildPath 'Unit' | Join-Path -ChildPath "$FunctionName.Tests.ps1"
+            $FunctionPath = Split-Path -Path $Function.FullName -Parent
+            if ($FunctionPath -match 'Public') {
+                $UnitType = 'Public'
+            } else {
+                $UnitType = 'Private'
+            }
+            $TestScriptPath = Join-Path -Path $BaseTestScriptPath -ChildPath $UnitType | Join-Path -ChildPath "$FunctionName.Tests.ps1"
             try {
                 $null = New-Item -Path $TestScriptPath -ItemType File -Force
-                $null = Set-Content -Path $TestScriptPath -Value $DefaultContent.Replace('MODULECOMMAND',$FunctionName).Replace('MODULENAME',$env:BHProjectName) -Encoding UTF8
+                $null = Set-Content -Path $TestScriptPath -Value $DefaultContent.Replace('MODULECOMMAND',$FunctionName).Replace('MODULENAME',$env:BHProjectName).Replace('UNITTYPE',$UnitType) -Encoding UTF8
                 Write-Verbose -Message "Created $TestScriptPath"
             }
             catch {
